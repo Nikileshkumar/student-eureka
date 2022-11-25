574a0448365df52a545e47dfa691ca450db5dbe3
nikileshkumar <nikileshkumar333@gmail.com>
2022-11-25 17:06:44 +0530

    Initial commit

---
+++ src/main/java/com/encora/student/StudentEurekaApplication.java
@@ -0,0 +1,16 @@
+package com.encora.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class StudentEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentEurekaApplication.class, args);
+	}
+
+}
